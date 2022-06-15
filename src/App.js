a9d95c076f77d5b67c09d9ab20c99a83055e58df
Brian <henkel.87@gmail.com>
2022-06-15 16:25:05 -0700

    added react-modern-drawer and react-icons.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import './App.css';
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css';
+import { GrGallery } from 'react-icons/gr';
 
 function App() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
-    <div className="App"> 
-      <img className="App-title" src="./img/splash.png" alt="splash"></img>
+    <div className='App'>
+      <Drawer size='25vw' open={isOpen} onClose={toggleDrawer} direction='left'>
+        <div className='Drawer'>hello world</div>
+      </Drawer>
+      <img className='App-title' src='/img/splash.png' alt='splash' />
+      <GrGallery className='Gallery-button' onClick={toggleDrawer} />
+      <div className='App-image-container'>
+        <img className='App-image' src='/img/image00.png' alt='image00' />
+      </div>
     </div>
   );
 }
