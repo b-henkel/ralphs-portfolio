a9d44e87db4ec26d15fc288a648cff9d9a795ede
Brian <henkel.87@gmail.com>
2022-06-23 14:39:32 -0700

    Added images.json file, added dynamic gallery creation, clicking images in gallery drawer changes image displayed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Drawer from 'react-modern-drawer';
 import 'react-modern-drawer/dist/index.css';
 import { GrGallery } from 'react-icons/gr';
+import imageData from './images.json';
 
 function App() {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
   };
 
+  const [imageShown, setImageShown] = useState(imageData.images[0].source);
+
   return (
     <div className='App'>
       <Drawer size='25vw' open={isOpen} onClose={toggleDrawer} direction='left'>
-        <div className='Drawer'>hello world</div>
+        <div className='Drawer'>
+          {imageData.catagories.map((item) => {
+            return (
+              <>
+                <h1>{item}</h1>
+                <ul className='App-catagory'>
+                  {imageData.images.map((image, index) => {
+                    if (imageData.images[index].catagory === item) {
+                      return (
+                        <li>
+                          <img
+                            src={imageData.images[index].source}
+                            alt={imageData.images[index].title}
+                            style={{ width: 150 }}
+                            onClick={() =>
+                              setImageShown(imageData.images[index].source)
+                            }
+                          ></img>
+                        </li>
+                      );
+                    }
+                  })}
+                </ul>
+              </>
+            );
+          })}
+        </div>
       </Drawer>
       <img className='App-title' src='/img/splash.png' alt='splash' />
       <GrGallery className='Gallery-button' onClick={toggleDrawer} />
       <div className='App-image-container'>
-        <img className='App-image' src='/img/image00.png' alt='image00' />
+        <img className='App-image' src={imageShown} alt='image00' />
       </div>
     </div>
   );
